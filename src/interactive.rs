0e8f53dd3d572cd881192cb1bae6840592d7a25a
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-01 17:37:09 -0600

    Add larger example

--- src/interactive.rs
+++ src/interactive.rs
@@ -3,13 +3,32 @@ use cursive::event::{Event, Key};
 use cursive::traits::*;
 use cursive::vec::Vec2;
 use cursive::view::View;
-use cursive::views::{Dialog, LinearLayout, OnEventView, Panel};
+use cursive::views::{Dialog, LinearLayout, OnEventView, Panel, ViewRef};
 use cursive::Cursive;
 use cursive::Printer;
 
 pub fn main() {
-    let row_clues = vec![vec![2], vec![1, 1], vec![3]];
-    let col_clues = vec![vec![3], vec![1, 1], vec![1], vec![1]];
+    let row_clues = vec![
+        vec![2],
+        vec![2, 1],
+        vec![1, 1],
+        vec![3],
+        vec![1, 1],
+        vec![1, 1],
+        vec![2],
+        vec![1, 1],
+        vec![1, 2],
+        vec![2],
+    ];
+    let col_clues = vec![
+        vec![2, 1],
+        vec![2, 1, 3],
+        vec![7],
+        vec![1, 3],
+        vec![2, 1],
+    ];
+    // let row_clues = vec![vec![2], vec![1, 1], vec![3]];
+    // let col_clues = vec![vec![3], vec![1, 1], vec![1], vec![1]];
     let mut non = Nonogram::new(row_clues, col_clues);
     non.set_tile(1, 2, Tile::Filled);
     non.set_tile(2, 2, Tile::NotFilled);
@@ -32,45 +51,39 @@ pub fn main() {
                 }),
         )
         .on_event(Event::Key(Key::Up), |s| {
-            s.find_id::<NonogramView>("game").unwrap().move_focus_up();
+            get_nonogram_view(s).move_focus_up();
         })
         .on_event(Event::Key(Key::Down), |s| {
-            s.find_id::<NonogramView>("game").unwrap().move_focus_down();
+            get_nonogram_view(s).move_focus_down();
         })
         .on_event(Event::Key(Key::Left), |s| {
-            s.find_id::<NonogramView>("game").unwrap().move_focus_left();
+            get_nonogram_view(s).move_focus_left();
         })
         .on_event(Event::Key(Key::Right), |s| {
-            s.find_id::<NonogramView>("game")
-                .unwrap()
-                .move_focus_right();
+            get_nonogram_view(s).move_focus_right();
         })
         .on_event(Event::Char('z'), |s| {
-            let mut non_view = s.find_id::<NonogramView>("game").unwrap();
+            let mut non_view = get_nonogram_view(s);
             non_view.toggle_filled_focused();
             if non_view.is_correct_solution() {
-                // eprintln!("Is correct solution!");
                 s.add_layer(
                     Dialog::new().title("You won!").button("Ok", |s| s.quit()),
                 );
-            } else {
-                // eprintln!("Not correct solution");
             }
-            // s.find_id::<NonogramView>("game")
-            //     .unwrap()
-            //     .toggle_filled_focused();
         })
         .on_event(Event::Char('x'), |s| {
-            s.find_id::<NonogramView>("game")
-                .unwrap()
-                .toggle_not_filled_focused();
+            get_nonogram_view(s).toggle_not_filled_focused();
         })
         .on_event(Event::Char('c'), |s| {
-            s.find_id::<NonogramView>("game").unwrap().clear_focused();
+            get_nonogram_view(s).clear_focused();
         }),
     );
 
     siv.run();
+}
+
+fn get_nonogram_view(s: &mut Cursive) -> ViewRef<NonogramView> {
+    s.find_id::<NonogramView>("game").unwrap()
 }
 
 pub struct NonogramView {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 pub mod nonogram;
 pub use crate::nonogram::{Nonogram, Tile};
 
+// cfg_if! {
+//     if #[cfg(feature = "interactive")] {
+//         mod interactive;
+//         fn main() {
+//             interactive::main();
+//         }
+//     } else {
+//         pub mod formatter;
+//         fn main() {
+//             formatter::main();
+//         }
+//     }
+// }
+
 #[cfg(not(feature = "interactive"))]
 pub mod formatter;
 
