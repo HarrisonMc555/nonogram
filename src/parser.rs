811b6f280f2a0cbdc3fe5eb86d83b082ea373d0e
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-10 08:35:45 -0600

    Add hex color parser

--- src/parser.rs
+++ src/parser.rs
@@ -26,6 +26,35 @@ named_one_line_key_string!(copyright);
 named_one_line_key_string!(goal);
 named_one_line_key_quoted_or_unquoted_string!(license);
 
+#[derive(Debug, PartialEq)]
+pub struct Color {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
+    u8::from_str_radix(input, 16)
+}
+
+fn is_hex_digit(c: char) -> bool {
+    c.is_digit(16)
+}
+
+named!(hex_primary<&str, u8>,
+       map_res!(take_while_m_n!(2, 2, is_hex_digit), from_hex)
+);
+
+named!(hex_color<&str, Color>,
+       do_parse!(
+           tag!("#") >>
+               red:   hex_primary >>
+               green: hex_primary >>
+               blue:  hex_primary >>
+               (Color { red, green, blue })
+       )
+);
+
 pub fn main() {
     assert_eq!(
         license(&b"license: \"A restrictive license\"\n"[..]),
@@ -134,5 +163,20 @@ fn parses_unquoted_license() {
     assert_eq!(
         license(&b"license: MIT-8.0\n"[..]),
         Ok((&[][..], "MIT-8.0"))
+    );
+}
+
+#[test]
+fn parses_color() {
+    assert_eq!(
+        hex_color("#2F14DF"),
+        Ok((
+            "",
+            Color {
+                red: 47,
+                green: 20,
+                blue: 223,
+            }
+        ))
     );
 }
